ce514dd6bebffae42c93e61cc17f974abf5b05c8
Briter Gonzalez <briter.gd@gmail.com>
2024-11-08 17:40:52 +0100

    remove additional file

--- src/server.js
+++ src/server.js
@@ -2,7 +2,39 @@ const express = require("express");
 require('dotenv').config()
 const bodyParser = require("body-parser");
 const port = 3000;
-const rabbitMQ = require('./rabbit-mq');
+
+
+const username = process.env.RABBITMQ_USERNAME;
+const password = process.env.RABBITMQ_PASSWORD;
+const service = process.env.RABBITMQ_SERVICE;
+
+const amqp = require("amqplib/callback_api");
+
+const initRabbitMQConnection = (callback) => {
+  amqp.connect(
+    `amqp://${username}:${password}@${service}`,
+    function (error0, connection) {
+      if (error0) {
+        throw error0;
+      }
+
+      /// 3 Establish a channel
+      connection.createChannel(function (error1, channel) {
+        if (error1) {
+          throw error1;
+        }
+        const queue_name = "first_queue";
+        const wrapper = {
+          send: (message) => {
+            channel.sendToQueue(queue_name, Buffer.from(message));
+          },
+        };
+        channel.assertQueue(queue_name, { durable: false });
+        callback(wrapper);
+      });
+    }
+  );
+};
 
 
 const initServer = (wrapper) => {
@@ -28,4 +60,4 @@ const initServer = (wrapper) => {
 }
 
 
-rabbitMQ(initServer);
+initRabbitMQConnection(initServer);
